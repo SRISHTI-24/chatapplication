7a50fa4384e22d6575f0c46a5ecfff9a0ae0b76c
SRISHTI SAXENA <73647667+SRISHTI-24@users.noreply.github.com>
2023-05-19 03:19:34 +0530

    first-commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+
+const http = require('http').createServer(app)
+const PORT = process.env.PORT || 2000;
+
+http.listen(PORT, () => {
+    console.log('server is running at port', PORT);
+})
+
+
+app.use(express.static(__dirname + '/public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+//socket
+
+const io = require('socket.io')(http)
+io.on('connection', (socket) => {
+    console.log('connected ..')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+})
